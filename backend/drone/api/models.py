f7928ce9590a49a01e57119ac0db83b931741a28
Soubhagya Nayak <144795594+Happytth@users.noreply.github.com>
2025-11-09 23:16:26 +0530

    Add API views and URL routing for main endpoint

--- backend/drone/api/models.py
+++ backend/drone/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    fullname=models.CharField(max_length=256, null=False, blank=False)
+    email_id=models.EmailField(unique=True, null=False, blank=False)
+    alternative_email_id=models.EmailField(unique=True, null=False, blank=False)
+    age=models.PositiveIntegerField(null=False, blank=False)
+    
+    position=models.CharField(max_length=10)
+    contact_no1=models.CharField(max_length=15, null=False, blank=False)
+    contact_no2=models.CharField(max_length=15, null=False, blank=False)
+    linkedin = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
+    gender = models.CharField(
+        max_length=10,
+        choices=[
+            ('Male', 'Male'),
+            ('Female', 'Female'),
+            ('Other', 'Other'),
+        ],
+        null=False,
+        blank=False
+    )
+    joined_on=models.DateField()
+    
+    
+    
