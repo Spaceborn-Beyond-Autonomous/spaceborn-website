9e8abcd7b24f9e7e67f04d2c1da8f5f51a1ce324
Soubhagya Nayak <abhiramnayak5000@gmail.com>
2025-11-10 10:08:30 +0530

    Add CORS support and update API view routing

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import User
 from rest_framework import generics
 from .serializers import UsersSerializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+# @get_view([GET])
 class UsersView(generics.CreateAPIView):
     users=User.objects.all()
     serializer_class = UsersSerializers
